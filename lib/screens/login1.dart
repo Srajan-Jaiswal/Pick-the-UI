bda3e4306eda535e669e18d78e8db17c28666fc7
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-09-25 13:50:08 +0530

    Update login1.dart

--- lib/screens/login1.dart
+++ lib/screens/login1.dart
@@ -166,3 +166,21 @@ class LinearGradientMask extends StatelessWidget {
 
   @override
  
+ Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcIn,
+      shaderCallback: (bounds) {
+        return LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: <Color>[
+            UIHelper.APPLE_GRADIENT_COLOR_ONE,
+            UIHelper.APPLE_GRADIENT_COLOR_TWO,
+          ],
+          tileMode: TileMode.mirror,
+        ).createShader(bounds);
+      },
+      child: child,
+    );
+  }
+}
