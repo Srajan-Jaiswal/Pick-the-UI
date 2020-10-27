94d63bfe650d8881b0d722e0223980a2d0cd6ee9
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-10-27 23:34:45 +0530

    Update text_styles.dart

--- lib/ui/styles/text_styles.dart
+++ lib/ui/styles/text_styles.dart
@@ -2,12 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_template_login/ui/ui_helper.dart';
 
 class UITextStyles {
-  static TextStyle loginStyle = TextStyle(
-      fontFamily: 'Montserrat',
-      color: UIHelper.WHITE,
-      fontSize: 45,
-      fontWeight: FontWeight.w500);
-
+  
   static TextStyle firstStyle = TextStyle(
       fontFamily: 'Montserrat',
       color: UIHelper.WHITE,
